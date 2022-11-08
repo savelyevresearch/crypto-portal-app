2bec540e45356598119aaa94392dcf4f1ceaacfa
Vadim <savelyevresearch@protonmail.com>
2022-11-08 18:44:05 +0600

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
+import { Switch, Route, Link } from 'react-router-dom';
+
+import { Layout, Typography, Space } from 'antd';
+
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
+import Exchanges from './components/Exchanges';
+
+import './App.css';
+
 const App = () => {
   return (
-    <div>
-        <h1>Cryptoverse</h1>
+    <div className='app'>
+        <div className='navbar'>
+            <Navbar />
+        </div>
+        <div className='main'>
+            <Layout>
+                <div className='routes'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Homepage />
+                        </Route>
+                        <Route exact path='/exchanges'>
+                            <Exchanges />
+                        </Route>
+                        <Route exact path='/cryptocurrencies'>
+                            <Cryptocurrencies />
+                        </Route>
+                        <Route exact path='/crypto/:coinId'>
+                            <CryptoDetails />
+                        </Route>
+                        <Route exact path='/news'>
+                            <News />
+                        </Route>
+                    </Switch>
+                </div>
+            </Layout>
+            <div className='footer'>
+                <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                    Cryptoverse <br />
+                    All rights reserverd
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
+
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+    <Router>
+        <App />
+    </Router>
+);
